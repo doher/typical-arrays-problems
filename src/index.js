265e66b11794a4886b91cadf3a3e7a5ed2f24768
doher <alexandr.pletezhov@gmail.com>
2020-02-21 10:56:50 +0300

    feat: add solution totypical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!Array.isArray(array)) {
+        return 0;
+    }
+
+    if (array.length <= 0) {
+        return 0;
+    }
+
+    return Math.min.apply(null, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!Array.isArray(array)) {
+        return 0;
+    }
+
+    if (array.length <= 0) {
+        return 0;
+    }
+
+    return Math.max.apply(null, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!Array.isArray(array)) {
+        return 0;
+    }
+
+    let length = array.length;
+
+    if (length <= 0) {
+        return 0;
+    }
+
+    let sum = array.reduce((sum, current) => sum + current, 0)
+
+    return sum / length;
 }
